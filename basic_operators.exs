60be2d3a05b9ec509932153f1b8d23eca2250c3c
BabyBabaBabu <iamkitaaresort@gmail.com>
2023-08-03 11:27:30 +0300

    Updated with various operators

--- basic_operators.exs
+++ basic_operators.exs
@@ -1,4 +1,4 @@
-# A basic script to highlight the various arithmetic operations in Elixir
+# A basic script to highlight the various basic operators in Elixir
 
 # We've seen
 IO.puts(10 * 2) # multiply
@@ -16,6 +16,27 @@ IO.puts(mylist -- mynewlist) # subtract, won't print anything in stdio but in ie
 lname = "Kitaa"
 fullname = "Alex " <> lname
 IO.puts("Alex " <> "#{lname}")
-IO.write("#{fullname}")
+IO.write("#{fullname}\n")
 
-# Boolean operators
+# Boolean operators, these expect something that evaluates to either true/false
+IO.puts(true and false) # & operator, returns false, both need to be true
+IO.puts(true or false) # or operator, returns true, one needs to be true
+IO.puts(true and is_atom(:alex))
+
+# Logical Operators, these allow arguments that are non-boolean, all arguments except false/nil will evaluate to ture
+IO.puts(1 || true) # logical or, returns 1
+IO.puts(2 && false) # logical and, returns false
+IO.puts(!true) # logical not, negates the value
+IO.puts(!nil) # logical not, negates the value, inverts it
+
+# Structural comparison applies to the underlying data structures/data types
+
+# Comparison operators
+IO.puts(1 == "one") # equality comparison
+IO.puts(2 != 'two') # non-equality comparison
+IO.puts(2 <= 3) # less than or equal
+IO.puts(20 >= 2) # greater than or equal
+IO.puts(1<2) # less than
+IO.puts(2>1) # greater than
+IO.puts(1 === 1.0) # strict equality comparison for ints/floats, here returns false
+IO.puts(1 !== 1.0) # strict non-equality comparison for ints/floats, here returns true
