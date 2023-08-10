43a0019f2212dcd1c4c58f6e634c7674fe0afe0d
BabyBabaBabu <iamkitaaresort@gmail.com>
2023-08-10 16:20:43 +0300

    added comments on the codereuse script

--- codereuse.exs
+++ codereuse.exs
@@ -4,12 +4,16 @@
 # # Alias the module so it can be called as Bar instead of Foo.Bar
 # alias Foo.Bar, as: Bar
 
-# # Require the module in order to use its macros
-# require Foo
-
 # # Import functions from Foo so they can be called without the `Foo.` prefix
 # import Foo
 
+# # Require the module in order to use its macros
+# require Foo
+
+# Invokes the custom code defined in Foo as an extension point
+# use Foo
+
+# require & use are typically used with macros
 
 # alias Elixir.Math, as: Math
 # IO.puts(Math.square(2))
