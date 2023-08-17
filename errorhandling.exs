9adcc3f8e49cd82d519c5e1327ae0bc69eeedbdb
BabyBabaBabu <iamkitaaresort@gmail.com>
2023-08-17 16:19:44 +0300

    updated the script

--- errorhandling.exs
+++ errorhandling.exs
@@ -28,4 +28,58 @@ end
 # take errors in Elixir literally:
 # they are reserved for unexpected and/or exceptional situations
 # never for controlling the flow of our code.
-# In case you actually need flow control constructs, "throws" should be used.
+# In case you actually need flow control constructs, "throw" should be used.
+
+# In Elixir, a value can be thrown and later be caught.
+try do
+  Enum.each(-50..50, fn x ->
+    if rem(x,13) == 0, do: throw(x) end)
+  IO.puts("Got nothing")
+catch
+  x -> IO.puts("Got #{x}")
+end
+
+# When a process dies of “natural causes” (e.g., unhandled exceptions), it sends an exit signal
+# exit signals are an important part of the fault tolerant system
+# supervisor processes listen for exit signals
+# Once an exit signal is received
+# the supervision strategy kicks in and the supervised process is restarted.
+
+try do
+  IO.puts("Hello World")
+  exit("I am exiting")
+catch
+  :exit, _ -> "Not really"
+end
+
+spawn_link( fn ->
+  IO.puts("Hello World!")
+  exit("Oops!") # returns ** (EXIT from #PID<0.116.0>) shell process exited with reason: "Oops!"
+end)
+
+# try/after
+# ensure that a resource is cleaned up after some action that could potentially raise an error.
+
+# e.g Open a file and use an after clause to close it – even if something goes wrong
+# "after" clause will be executed regardless of whether or not the "try" block succeeds.
+{:ok, file} = File.open("file.txt", [:write, :utf8])
+try do
+  IO.write(file, "Hola")
+  raise "Oops, something went wrong!"
+after
+  File.close(file)
+end
+
+# use of raise, catch, after in a function body
+# Will cause Elixir to automatically wrap the function body in a try
+# prints the hello world, and the message in the after before raising the error
+defmodule RunAfter do
+  def try_this do
+    IO.puts("Hello World")
+    raise RuntimeError, message: "Oops,something went wrong :-("
+  after
+    IO.puts("Cleaning up...")
+  end
+end
+
+# variables in try, catch, raise do & after blocks are locally scoped, they do not leak
